f2d18405ed25082c08e3f701e473af76a5ea8a9f
Llll0l0l <h0cker.hack1@gmail.com>
2023-05-04 22:04:50 +0400

    80% done only the reset part and some styling is left

--- index.js
+++ index.js
@@ -46,22 +46,108 @@ function changeTurn(event) {
 
     if (!target.textContent) {
         if (count % 2 == 0) {
-            turnText.innerHTML = `${player1}'s turn`;
+            turnText.innerHTML = `${player2}'s turn`;
             target.innerHTML = "<span>X</span>";
 
         } else {
-            turnText.innerHTML = `${player2}'s turn`;
+            turnText.innerHTML = `${player1}'s turn`;
             target.innerHTML = "<span>O</span>";
         }
 
         target.classList.add("xod");
         count++;
+        gameOver(event);
     }
+
 }
 
 // increase the count on click the divs of xox
 const tto = document.getElementsByClassName("grid");
 console.dir(tto);
 // make an arrow event listener
-
+if (count % 2 == 0) {
+    turnText.innerHTML = `${player1}'s turn`;
+}
 tto[0].addEventListener("click", changeTurn);
+
+
+
+// function to check whether the game is over or not
+function gameOver(event) {
+    const div1 = document.getElementById("1").textContent;
+    const div2 = document.getElementById("2").textContent;
+    const div3 = document.getElementById("3").textContent;
+    const div4 = document.getElementById("4").textContent;
+    const div5 = document.getElementById("5").textContent;
+    const div6 = document.getElementById("6").textContent;
+    const div7 = document.getElementById("7").textContent;
+    const div8 = document.getElementById("8").textContent;
+    const div9 = document.getElementById("9").textContent;
+
+    const winnerName = document.getElementsByClassName("winner");
+
+
+    if (div1 != "" && div1 == div2 && div2 == div3) {
+        // change the wiiner name to the player's name
+        tto[0].removeEventListener("click", changeTurn);
+        if (div1 == 'X') {
+            winnerName[0].innerHTML = `${player1} won`;
+        } else {
+            winnerName[0].innerHTML = `${player2} won`;
+        }
+    } else if (div1 != "" && div1 == div4 && div4 == div7) {
+        tto[0].removeEventListener("click", changeTurn);
+
+        if (div1 == 'X') {
+            winnerName[0].innerHTML = `${player2} won`;
+        } else {
+            winnerName[0].innerHTML = `${player1} won`;
+        }
+    } else if (div1 != "" && div1 == div5 && div5 == div9) {
+        tto[0].removeEventListener("click", changeTurn);
+        if (div1 == 'X') {
+            winnerName[0].innerHTML = `${player2} won`;
+        } else {
+            winnerName[0].innerHTML = `${player1} won`;
+        }
+    } else if (div2 != "" && div2 == div5 && div5 == div8) {
+        tto[0].removeEventListener("click", changeTurn);
+        if (div1 == 'X') {
+            winnerName[0].innerHTML = `${player2} won`;
+        } else {
+            winnerName[0].innerHTML = `${player1} won`;
+        }
+    } else if (div3 != "" && div3 == div6 && div6 == div9) {
+        tto[0].removeEventListener("click", changeTurn);
+        if (div1 == 'X') {
+            winnerName[0].innerHTML = `${player2} won`;
+        } else {
+            winnerName[0].innerHTML = `${player1} won`;
+        }
+    } else if (div4 != "" && div4 == div5 && div5 == div6) {
+        tto[0].removeEventListener("click", changeTurn);
+        if (div1 == 'X') {
+            winnerName[0].innerHTML = `${player2} won`;
+        } else {
+            winnerName[0].innerHTML = `${player1} won`;
+        }
+    } else if (div7 != "" && div7 == div8 && div8 == div9) {
+        tto[0].removeEventListener("click", changeTurn);
+        if (div1 == 'X') {
+            winnerName[0].innerHTML = `${player2} won`;
+        } else {
+            winnerName[0].innerHTML = `${player1} won`;
+        }
+    }
+
+
+}
+
+
+// if game is over disable the click event listener on the parent
+
+// change the you won name part to the player's name
+
+
+// fix the start new game button so that it displays the game
+// add a clear the board function
